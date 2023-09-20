13ad7a85945d665e47a9f4d9942d72ef17682a02
awe0w0 <hanser0w0@gmail.com>
2023-09-20 11:53:07 +0800

    fix projectile hart

--- Source/UEActionGame/Private/MainMagicProjectile.cpp
+++ Source/UEActionGame/Private/MainMagicProjectile.cpp
@@ -46,7 +46,7 @@ void AMainMagicProjectile::Tick(float DeltaTime)
 }
 
 void AMainMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
-	if (OtherActor && OtherActor != GetInstigator()) {
+	if (OtherActor) {
 		UMainAttributesComponent* AttributeComp = Cast<UMainAttributesComponent>(OtherActor->GetComponentByClass(UMainAttributesComponent::StaticClass()));
 		if (AttributeComp) {
 			AttributeComp->ApplyHealthChange(-20.0f);
