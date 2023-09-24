3532aecbacd8fa43de44ecc38f9194cce10bfa38
awe0w0 <hanser0w0@gmail.com>
2023-09-25 01:26:52 +0800

    fix damage ui spawn rule, but still have some bugs

--- Source/UEActionGame/Private/MainTargetDummy.cpp
+++ Source/UEActionGame/Private/MainTargetDummy.cpp
@@ -12,11 +12,8 @@ AMainTargetDummy::AMainTargetDummy()
 	RootComponent = MeshComp;
 
 	AttributeComp = CreateDefaultSubobject<UMainAttributesComponent>("AttributeComp");
-	//Trigger when health is changed
+	// Trigger when health is changed (damage/healing)
 	AttributeComp->OnHealthChanged.AddDynamic(this, &AMainTargetDummy::OnHealthChanged);
-
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
 
 }
 
